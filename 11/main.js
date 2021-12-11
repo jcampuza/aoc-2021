d020162a1cee66e7d8b9dd34de0bb7287cae4cd2
Joseph Campuzano <joseph@dialexa.com>
2021-12-11 12:10:36 -0600

    remove unused js

--- 11/main.js
+++ 11/main.js
@@ -11,39 +11,6 @@ const increment = (m, x, y) => {
   }
 };
 
-/**
- *
- * @param {number[][]} m
- * @param {Set<string>} s
- * @returns
- */
-const flashPass = (m, s) => {
-  let flashCount = 0;
-  for (let y = 0; y < m.length; y++) {
-    for (let x = 0; x < m.length; x++) {
-      if (m[x][y] > 9 && !s.has(`${x}${y}`)) {
-        s.add(`${x}${y}`);
-        flashCount++;
-        // Left
-        increment(m, x - 1, y - 1);
-        increment(m, x - 1, y);
-        increment(m, x - 1, y + 1);
-
-        // middle
-        increment(m, x, y - 1);
-        increment(m, x, y + 1);
-
-        // Right
-        increment(m, x + 1, y - 1);
-        increment(m, x + 1, y);
-        increment(m, x + 1, y + 1);
-      }
-    }
-  }
-
-  return flashCount;
-};
-
 const step = (m) => {
   // Increment
   for (let y = 0; y < m.length; y++) {
@@ -101,7 +68,6 @@ const step = (m) => {
 
 const partOne = (input = '') => {
   const m = input.split('\n').map((line) => line.split('').map(Number));
-
   return Array.from({ length: 100 }).reduce((acc) => acc + step(m), 0);
 };
 
