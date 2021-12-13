247cbc840edefe4e63f467a0af60ce41ac01aaca
Joseph Campuzano <joseph@dialexa.com>
2021-12-13 12:22:26 -0600

    day 13 js

---
+++ 13/main.js
@@ -0,0 +1,91 @@
+const fs = require('fs');
+const path = require('path');
+
+const readInput = (file = '') => {
+  return fs.readFileSync(file, 'utf-8');
+};
+
+const logPlot = (plot) => {
+  plot.forEach((line) => console.log(line.join('')));
+};
+
+const iterateMatrix = (matrix, cb) => {
+  for (let y = 0; y < matrix.length; y++) {
+    for (let x = 0; x < matrix[y].length; x++) {
+      const value = matrix[y][x];
+      cb({ x, y, value }, matrix);
+    }
+  }
+};
+
+const foldPlot = (plot, fold) => {
+  if (fold.axis === 'y') {
+    iterateMatrix(plot, (point) => {
+      if (point.y > fold.value && point.value === '#') {
+        plot[point.y - (point.y - fold.value) * 2][point.x] = point.value;
+      }
+    });
+
+    return plot.slice(0, fold.value);
+  }
+
+  if (fold.axis === 'x') {
+    iterateMatrix(plot, (point) => {
+      if (point.x > fold.value && point.value === '#') {
+        plot[point.y][point.x - (point.x - fold.value) * 2] = point.value;
+      }
+    });
+
+    return plot.map((line) => line.slice(0, fold.value));
+  }
+};
+
+const parseFoldsAndPlot = (input) => {
+  const [dotsRaw, foldsRaw] = input.split('\n\n');
+
+  const dots = dotsRaw
+    .split('\n')
+    .map((line) => line.split(',').map(Number))
+    .map(([x, y]) => ({ x, y }));
+  const maxX = Math.max(...dots.map((dot) => dot.x));
+  const maxY = Math.max(...dots.map((dot) => dot.y));
+
+  const folds = foldsRaw
+    .split('\n')
+    .map((fold) => fold.split('fold along ')[1])
+    .map((fold) => fold.split('='))
+    .map(([axis, value]) => ({ axis, value: Number(value) }));
+
+  const plot = Array.from({ length: maxY + 1 }, () => Array.from({ length: maxX + 1 }, () => '.'));
+  dots.forEach((dot) => (plot[dot.y][dot.x] = '#'));
+
+  return { plot, folds };
+};
+
+const partOne = (input = '') => {
+  let { plot, folds } = parseFoldsAndPlot(input);
+  plot = foldPlot(plot, folds[0]);
+
+  let count = 0;
+  iterateMatrix(plot, (point) => point.value === '#' && count++);
+
+  return count;
+};
+
+const partTwo = (input = '') => {
+  const { plot, folds } = parseFoldsAndPlot(input);
+  const result = folds.reduce((acc, fold) => foldPlot(acc, fold), plot);
+
+  logPlot(result);
+};
+
+const t = readInput(path.join(__dirname, 'test.txt'));
+console.log('------------TEST-----------');
+console.log('Part One:', partOne(t));
+console.log('Part Two:', partTwo(t));
+console.log('\n');
+
+const d = readInput(path.join(__dirname, 'input.txt'));
+console.log('------------REAL-----------');
+console.log('Part One:', partOne(d));
+console.log('Part Two:', partTwo(d));
